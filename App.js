01922d4893aea06fc9f7ae7c6483e1a84ae92ddb
Ian Hansen <221826+supershabam@users.noreply.github.com>
2022-07-20 11:28:32 -0600

    able to insert an item

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [db, setDB] = useState(null)
   const [ok, setOk] = useState(false)
   const [status, setStatus] = useState('')
+
+  const onPressInsert = () => {
+    if (!db) {
+      console.log('db not set, skipping!')
+      return
+    }
+    db.exec([
+      {
+        sql: `INSERT INTO hello_worlds (at) VALUES (1234)`,
+        args: []
+      }
+    ], false, (err, result) => {
+      console.log('press')
+      console.log(err)
+      console.log(result)
+      db.exec([
+        {
+          sql: `SELECT * FROM hello_worlds`,
+          args: []
+        }], true, (err, result) => {
+          console.log('read')
+          console.log(err)
+          console.log(result)
+        }
+      )
+    })
+  }
 
   useEffect(() => {
     let run = async () => {
       const db = SQLite.openDatabase('greetings.db')
-      const create = new Promise((resolve, reject) => {
+      const create = await new Promise((resolve, reject) => {
         db.exec([
           {
             sql: `
@@ -26,11 +54,12 @@ export default function App() {
           resolve(result)
         })
       })
-      return create
+      setDB(db)
+      return "ah yeah"
     }
-    run().then(()=> {
+    run().then((status)=> {
       setOk(true)
-      setStatus("run completed")
+      setStatus(status)
     }, (err) => {
       setOk(false)
       setStatus(`err=${err}`)
@@ -40,6 +69,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ok={ok} status={status}</Text>
+      <Button
+        onPress={onPressInsert}
+        title="Insert"
+        color="#841584"
+        accessibilityLabel="Insert item into database"
+/>
+
       <StatusBar style="auto" />
     </View>
   );
