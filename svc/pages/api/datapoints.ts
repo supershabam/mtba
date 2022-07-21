67d330013922bb25afba25ea9da7f34fcc2db913
Ian Hansen <221826+supershabam@users.noreply.github.com>
2022-07-21 13:04:04 -0600

    adds datapoints

--- svc/pages/api/datapoints.ts
+++ svc/pages/api/datapoints.ts
@@ -1,11 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 type Data = {
-  ats: Number[]
+  ats: number[]
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -13,7 +16,13 @@ export default function handler(
     res.status(404)
     return
   }
-  const ats: Number[] = req.body.ats
+  const ats: number[] = req.body.ats
   console.log(ats)
+  const response = await prisma.datapoints.createMany({
+    data: ats.map((at) => {
+      return {at: new Date(at)}
+    }),
+    skipDuplicates: true
+  })
   res.status(200).json({ats: ats})
 }
